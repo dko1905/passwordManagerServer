c05782410490bd6d5868accf5856eadca2f817fd
Daniel Florescu <daniel.florescu1905@protonmail.ch>
2020-06-29 12:04:48 +0200

    Backup

--- src/main/kotlin/io/github/dko1905/passwordmanagerserver/domain/Account.kt
+++ src/main/kotlin/io/github/dko1905/passwordmanagerserver/domain/Account.kt
@@ -1,3 +1,3 @@
 package io.github.dko1905.passwordmanagerserver.domain
 
-class Account(val ID: Long?, val USERNAME: String, val HASH: String, val ACCOUNTROLE: AccountRole)
+class Account(var ID: Long?, val USERNAME: String, val HASH: String, val ACCOUNTROLE: AccountRole)
--- src/main/kotlin/io/github/dko1905/passwordmanagerserver/repository/AccountRepositorySQLiteImpl.kt
+++ src/main/kotlin/io/github/dko1905/passwordmanagerserver/repository/AccountRepositorySQLiteImpl.kt
@@ -1,6 +1,7 @@
 package io.github.dko1905.passwordmanagerserver.repository
 
 import io.github.dko1905.passwordmanagerserver.domain.Account
+import io.github.dko1905.passwordmanagerserver.domain.AccountRole
 import org.springframework.beans.factory.annotation.Autowired
 import java.sql.SQLException
 import javax.sql.DataSource
@@ -15,7 +16,7 @@ class AccountRepositorySQLiteImpl(@Autowired val dataSource: DataSource) : Accou
                 preparedStatement.setInt(3, account.ACCOUNTROLE.value);
                 preparedStatement.execute();
             }
-            connection.prepareStatement("SELECT ID FROM ACCOUNT WHERE USERNAME=? AND HASH=? AND ROLE=?").use { preparedStatement ->
+            connection.prepareStatement("SELECT ID FROM ACCOUNT WHERE USERNAME=? AND HASH=? AND ROLE=?;").use { preparedStatement ->
                 preparedStatement.setString(1, account.USERNAME);
                 preparedStatement.setString(2, account.HASH);
                 preparedStatement.setInt(3, account.ACCOUNTROLE.value);
@@ -32,19 +33,62 @@ class AccountRepositorySQLiteImpl(@Autowired val dataSource: DataSource) : Accou
     }
 
     override fun removeAccount(id: Long) {
-        TODO("Not yet implemented")
+        dataSource.connection.use { connection ->
+            connection.prepareStatement("DELETE FROM ACCOUNT WHERE ID=?;").use { preparedStatement ->
+                preparedStatement.setLong(1, id);
+                preparedStatement.execute();
+            }
+        }
     }
 
     override fun replaceAccount(account: Account) {
-        TODO("Not yet implemented")
+        dataSource.connection.use { connection ->
+            connection.prepareStatement("UPDATE ACCOUNT SET USERNAME=?, HASH=?, ROLE=? WHERE ID=?;").use { preparedStatement ->
+                preparedStatement.setString(1, account.USERNAME);
+                preparedStatement.setString(2, account.HASH);
+                preparedStatement.setInt(3, account.ACCOUNTROLE.value);
+                preparedStatement.setLong(4, account.ID!!);
+                preparedStatement.execute();
+            }
+        }
     }
 
     override fun getAccount(id: Long): Account? {
-        TODO("Not yet implemented")
+        dataSource.connection.use { connection ->
+            connection.prepareStatement("SELECT ID,USERNAME,HASH,ROLE FROM ACCOUNT WHERE ID=?;").use { preparedStatement ->
+                preparedStatement.setLong(1, id);
+                preparedStatement.executeQuery().use { resultSet ->
+                    return if(resultSet.next()){
+                        Account(
+                                resultSet.getLong(1),
+                                resultSet.getString(2),
+                                resultSet.getString(3),
+                                AccountRole.fromInt(resultSet.getInt(4))
+                        )
+                    } else{
+                        null // Could not find result
+                    }
+                }
+            }
+        }
     }
 
-    override fun getAccounts(): ArrayList<Account> {
-        TODO("Not yet implemented")
-    }
+    override fun Ja(): ArrayList<Account> {
+        val accounts: ArrayList<Account> = ArrayList();
 
+        dataSource.connection.use { connection ->
+            connection.prepareStatement("SELECT ID,USERNAME,HASH,ROLE FROM ACCOUNT;").use { preparedStatement ->
+                preparedStatement.executeQuery().use { resultSet ->
+                    while(resultSet.next()){
+                        val ID = resultSet.getLong(1)
+                        val USERNAME = resultSet.getString(2)
+                        val PASSWORD = resultSet.getString(3)
+                        val ACCOUNTROLE = AccountRole.fromInt(resultSet.getInt(4))
+                        accounts.add(Account(ID, USERNAME, PASSWORD, ACCOUNTROLE))
+                    }
+                }
+            }
+        }
+        return accounts
+    }
 }
